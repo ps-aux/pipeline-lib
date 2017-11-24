269a107e29863a40200b783e1c82b7b489d6ff8c
ps-aux <seriouz.accz@gmail.com>
2017-11-24 18:03:30 +0100

    Init

---
+++ src/aux/ps/Foo.groovy
@@ -0,0 +1,18 @@
+def foo (){
+    echo 'Fooooooooooo'
+}
+
+def commitInfo () {
+    def c = new LinkedHashMap();
+    c.author = gitInfo('%an')
+    c.message = gitInfo('%s')
+    c.time = gitInfo('%cr')
+
+    return c
+}
+
+def gitInfo (format) {
+    return  sh(returnStdout: true,
+            script: "git --no-pager show -s --format=${format}").trim()
+
+}
