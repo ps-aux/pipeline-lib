53b9d8b0422b788eb94a3ef9db7d1cdc06794f67
ps-aux <seriouz.accz@gmail.com>
2017-11-24 19:15:47 +0100

    Playing with...

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -4,7 +4,7 @@ def failed () {
     def commitText = """Last commit by *${commit.author}*
                         | ${commit.message}
                         | ${commit.time}
-                         """
+                         """.stripMargin()
 
-    return "Build *<${currentBuild.absoluteUrl}|${currentBuild.fullDisplayName}>* failed \n." + commitText
+    return "Build *<${currentBuild.absoluteUrl}|${currentBuild.fullDisplayName}>* failed \n" + commitText
 }
